d16ff2ce35d651821750742787bcdba32996f96c
SHANGEETH <shanshangeeth@gmail.com>
2023-02-06 19:11:15 +0530

    coin

---
+++ client/src/pages/Transactions/DepositModal.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Modal, Form, message } from "antd";
+import StripeCheckout from "react-stripe-checkout";
+import { DepositFunds } from "../../apicalls/transactions";
+import { useDispatch } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
+
+function DepositModal({ showDepositModal, setShowDepositModal, reloadData }) {
+  const [form] = Form.useForm();
+  const dispatch = useDispatch();
+  const onToken = async (token) => {
+    console.log(token);
+    try {
+      dispatch(ShowLoading());
+      const response = await DepositFunds({
+        token,
+        amount: form.getFieldValue("amount"),
+      });
+      dispatch(HideLoading());
+      if (response.success) {
+        reloadData();
+        setShowDepositModal(false);
+        message.success(response.message);
+      } else {
+        message.error(response.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  return (
+    <Modal
+      title="Deposit"
+      open={showDepositModal}
+      onCancel={() => setShowDepositModal(false)}
+      footer={null}
+    >
+      <div className="flex-col gap-1">
+        <Form layout="vertical" form={form}>
+          <Form.Item
+            label="Amount"
+            name="amount"
+            rules={[
+              {
+                required: true,
+                message: "Please input amount",
+              },
+            ]}
+          >
+            <input type="number" />
+          </Form.Item>
+
+          <div className="flex justify-end gap-1">
+            <button className="primary-outlined-btn">Cancel</button>
+            <StripeCheckout
+              token={onToken}
+              currency="USD"
+              amount={form.getFieldValue("amount") * 100}
+              stripeKey="pk_test_51MXL8ISDvR6Tbiedk5EoTKVQf9Ji4xVsTQS1B8AM9Hy9kVhceh0H7lKKqk8C4uGvWkDvDkow92cWcA0LJWQ0mR1T00y37nESSN"
+            >
+              <button className="primary-contained-btn">Deposit</button>
+            </StripeCheckout>
+          </div>
+        </Form>
+      </div>
+    </Modal>
+  );
+}
+
+export default DepositModal;
