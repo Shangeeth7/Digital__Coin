0d2dd5584b21af3c05234435d2e9d31c5a724412
SHANGEETH <shanshangeeth@gmail.com>
2023-02-07 16:28:57 +0530

    render code

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ app.use("/api/requests", requestsRoute);
 const PORT = process.env.PORT || 1121;
 const path = require("path");
 __dirname = path.resolve();
-// heroku deployment
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
